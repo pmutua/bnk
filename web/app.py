65744d725c72f64de31363fefd25c1b34d76f7a0
philip <pmutua@live.com>
2020-07-13 11:23:07 +0300

    Add register resources in routes

--- web/app.py
+++ web/app.py
@@ -253,11 +253,11 @@ class PayLoan(Resource):
 
 
 api.add_resource(Register,'/register')
-api.add_resource(Register,'/add')
-api.add_resource(Register,'/transfer')
-api.add_resource(Register,'/balance')
-api.add_resource(Register,'/takeloan')
-api.add_resource(Register,'/payloan')
+api.add_resource(Add,'/add')
+api.add_resource(Transfer,'/transfer')
+api.add_resource(Balance,'/balance')
+api.add_resource(TakeLoan,'/takeloan')
+api.add_resource(PayLoan,'/payloan')
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
